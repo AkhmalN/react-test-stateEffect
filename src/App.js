4f211cf618a9c416cac3c5e04b95b2f4a1dc6970
AkhmalNovanda <cocwar427@gmail.com>
2023-01-17 21:40:06 +0700

    state effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
+
+  // TODO : useState()
+
+  // const [count, setCount] = useState(0)
+  // const [theme, setTheme] = useState('Normal')
+  // function incrementCount(){
+  //   setCount(count + 3)
+  //   setTheme(' + ')
+  // }
+  // function decrementCount(){
+  //   setCount(count-3)
+  //   setTheme(' - ')
+  // }
+  // console.log(count);
+  // const [inputValue, setInputValue] = useState()
+
+  // const [inputValue, setInputValue] = useState(0)
+
+  // const Change =(event) => {
+  //   const newValue = event.target.value
+  //   setInputValue(newValue)
+  // }
+  // const [numValue, setAddNum] = useState(0)
+  // const [textValue, setToAddText] = useState('add Some One')
+
+  //TODO : useEffect
+
+  const [resourceType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+  }, [resourceType])
+  // const [data, setData] = useState([])
+  // useEffect( ()=>{
+  //   setData([
+  //     {
+  //       name: 'person1'
+  //     },
+  //     {
+  //       name: 'person2'
+  //     }
+  //   ])
+  // })
+  // useEffect( () => {
+  //   fetch(`https://jsonplaceholder.typicode.com/users`)
+  //   .then(response=> response.json())
+  //   .then(json => setData(json))
+  // }, [data])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* ----- use State -------------}
+
+      {/* <button onClick={decrementCount}> - </button>
+      <span> {count} </span>
+      <span> {theme} </span>
+      <button onClick={incrementCount}> + </button> */}
+      {/* <h2>Use State</h2>
+      <input placeholder='Add SomeOne' onChange={Change} />
+      <br/>
+      {inputValue}
+      <br/>
+
+      <h1>{numValue}</h1>
+      <button onClick={() => {
+        setAddNum(numValue + 1)
+        setToAddText('number added')
+      }}
+      >
+        Add Num
+      </button>
+      <p>{textValue}</p> */}
+
+      {/* ------------ use Effect ---------- */}
+      <button onClick={() => setResourceType('posts')}>Posts</button>
+      <button onClick={() => setResourceType('users')}>Users</button>
+      <button onClick={() => setResourceType('comments')}>comments</button>
+      <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{ JSON.stringify(item) }</pre>
+      })}
+      {/* {data?.map( (el)=> {
+        return <p>{el?.name}</p>
+      })} */}
+      {/* {data.map( idx => {
+        return <div>{ JSON.stringify(idx) }</div>
+      })} */}
     </div>
   );
 }
